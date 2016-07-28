1b7f6eee528b0483207052e859f167652b33e14b
averis-jemmy <rooney.christiansen@gmail.com>
2016-07-28 11:57:45 +0800

    Add files via upload

--- learn/App.jsx
+++ learn/App.jsx
@@ -7,105 +7,240 @@ import DayPicker, { DateUtils } from "react-day-picker";
 
 class App extends React.Component {
 	constructor() {
-	  super();
-	  var _this = this;
-	  
-	  this.state = {
-		 selectedDays: [ new Date(), new Date() ],
-		 selectedDay: new Date(),
-         data: {"club":{"id":1,"name":"Kelab Darul Ehsan"},"flights":[{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"07:00","booked":1,"matrix_id":1,"reserve_status":1,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"07:15","booked":null,"matrix_id":2,"reserve_status":1,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"07:30","booked":null,"matrix_id":3,"reserve_status":2,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"07:45","booked":null,"matrix_id":4,"reserve_status":null,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"08:00","booked":null,"matrix_id":5,"reserve_status":null,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"08:30","booked":null,"matrix_id":6,"reserve_status":null,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":2,"maxPax":4,"minCart":0,"maxCart":2,"minCaddy":0,"maxCaddy":2,"tee_time":"09:00","booked":null,"matrix_id":7,"reserve_status":null,"prices":{"flight":"90.0","cart":"100.0","caddy":"90.0","insurance":"90.0","note":"","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"16:00","booked":null,"matrix_id":14,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"16:30","booked":null,"matrix_id":15,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"17:00","booked":null,"matrix_id":16,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"17:30","booked":null,"matrix_id":17,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"18:00","booked":null,"matrix_id":18,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"18:30","booked":null,"matrix_id":19,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}},{"minPax":3,"maxPax":4,"minCart":1,"maxCart":2,"minCaddy":1,"maxCaddy":2,"tee_time":"19:00","booked":null,"matrix_id":20,"reserve_status":null,"prices":{"flight":"180.0","cart":"100.0","caddy":"200.0","insurance":"50.0","note":"Free:-\r\n* drinks","insurance_mode":1}}],"queryData":{"date":"19/07/2016","query":""}}
-      }
-	  
-	  $.getJSON( "data.json", function( data ) {
-		  _this.setState({data: data})
-	  });
+		super();
+		var _this = this;
+
+		this.state = {
+			selectedDays: null,
+			selectedDay: new Date(),
+			data: null
+		}
+
+		$.getJSON( "data.json", function( data ) {
+			_this.setState({data: data})
+		});
 	}
 	
 	handleDayClick(e, day) {
 		this.setState({ selectedDay: day });
 		var date = new Date(day);
+		switch(day.getDay()) {
+			case 0:
+				this.selectedDays: [ { "day": "Monday", "date": date.setDate(date.getDate() + (-6)) },
+					{ "day": "Tuesday", "date": date.setDate(date.getDate() + (-5)) },
+					{ "day": "Wednesday", "date": date.setDate(date.getDate() + (-4)) },
+					{ "day": "Thursday", "date": date.setDate(date.getDate() + (-3)) },
+					{ "day": "Friday", "date": date.setDate(date.getDate() + (-2)) },
+					{ "day": "Saturday", "date": date.setDate(date.getDate() + (-1)) },
+					{ "day": "Sunday", "date": date }
+				]
+				break;
+			case 1:
+				this.selectedDays: [ { "day": "Monday", "date": date.setDate(date.getDate() + (-5)) },
+					{ "day": "Tuesday", "date": date.setDate(date.getDate() + (-4)) },
+					{ "day": "Wednesday", "date": date.setDate(date.getDate() + (-3)) },
+					{ "day": "Thursday", "date": date.setDate(date.getDate() + (-2)) },
+					{ "day": "Friday", "date": date.setDate(date.getDate() + (-1)) },
+					{ "day": "Saturday", "date": date },
+					{ "day": "Sunday", "date": date.setDate(date.getDate() + (1)) }
+				]
+				break;
+			case 2:
+				this.selectedDays: [ { "day": "Monday", "date": date.setDate(date.getDate() + (-4)) },
+					{ "day": "Tuesday", "date": date.setDate(date.getDate() + (-3)) },
+					{ "day": "Wednesday", "date": date.setDate(date.getDate() + (-2)) },
+					{ "day": "Thursday", "date": date.setDate(date.getDate() + (-1)) },
+					{ "day": "Friday", "date": date },
+					{ "day": "Saturday", "date": date.setDate(date.getDate() + (1)) },
+					{ "day": "Sunday", "date": date.setDate(date.getDate() + (2)) }
+				]
+				break;
+			case 3:
+				this.selectedDays: [ { "day": "Monday", "date": date.setDate(date.getDate() + (-3)) },
+					{ "day": "Tuesday", "date": date.setDate(date.getDate() + (-2)) },
+					{ "day": "Wednesday", "date": date.setDate(date.getDate() + (-1)) },
+					{ "day": "Thursday", "date": date },
+					{ "day": "Friday", "date": date.setDate(date.getDate() + (1)) },
+					{ "day": "Saturday", "date": date.setDate(date.getDate() + (2)) },
+					{ "day": "Sunday", "date": date.setDate(date.getDate() + (3)) }
+				]
+				break;
+			case 4:
+				this.selectedDays: [ { "day": "Monday", "date": date.setDate(date.getDate() + (-2)) },
+					{ "day": "Tuesday", "date": date.setDate(date.getDate() + (-1)) },
+					{ "day": "Wednesday", "date": date },
+					{ "day": "Thursday", "date": date.setDate(date.getDate() + (1)) },
+					{ "day": "Friday", "date": date.setDate(date.getDate() + (2)) },
+					{ "day": "Saturday", "date": date.setDate(date.getDate() + (3)) },
+					{ "day": "Sunday", "date": date.setDate(date.getDate() + (4)) }
+				]
+				break;
+			case 5:
+				this.selectedDays: [ { "day": "Monday", "date": date.setDate(date.getDate() + (-1)) },
+					{ "day": "Tuesday", "date": date },
+					{ "day": "Wednesday", "date": date.setDate(date.getDate() + (1)) },
+					{ "day": "Thursday", "date": date.setDate(date.getDate() + (2)) },
+					{ "day": "Friday", "date": date.setDate(date.getDate() + (3)) },
+					{ "day": "Saturday", "date": date.setDate(date.getDate() + (4)) },
+					{ "day": "Sunday", "date": date.setDate(date.getDate() + (5)) }
+				]
+				break;
+			case 6:
+				this.selectedDays: [ { "day": "Monday", "date": date },
+					{ "day": "Tuesday", "date": date.setDate(date.getDate() + (1)) },
+					{ "day": "Wednesday", "date": date.setDate(date.getDate() + (2)) },
+					{ "day": "Thursday", "date": date.setDate(date.getDate() + (3)) },
+					{ "day": "Friday", "date": date.setDate(date.getDate() + (4)) },
+					{ "day": "Saturday", "date": date.setDate(date.getDate() + (5)) },
+					{ "day": "Sunday", "date": date.setDate(date.getDate() + (6)) }
+				]
+				break;
+			default:
+				break;
+		}
 	}
 
-	render() {	  
-	  return (
-		 <div>
-			<DayPicker
-				onDayClick={ this.handleDayClick.bind(this) }
-				selectedDays={ day => DateUtils.isSameDay(day, this.state.selectedDay) }
-			/>
-			<Table striped bordered condensed>
-				<thead>
-					<tr>
-						<th></th>
-						{this.state.data.flights.map((flight, i) => <TableHeadColumn key = {i} data = {flight} />)}
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<TableRowHead headerProp = {this.state.data.queryData.date} />
-						{this.state.data.flights.map((flight, i) => <TableRowColumn key = {i} data = {flight} />)}
-					</tr>
-				</tbody>
-			</Table>
-		 </div>
-	  );
+	render() {
+		if(this.state.data == null || this.state.selectedDays == null) {
+			return (
+			 <div>
+				
+			 </div>
+			);
+		}
+		  
+		return (
+			<div>
+				<DayPicker
+					onDayClick={ this.handleDayClick.bind(this) }
+					selectedDays={ day => DateUtils.isSameDay(day, this.state.selectedDay) }
+				/>
+				<Table striped bordered condensed>
+					<thead>
+						<tr>
+							<th></th>
+							{this.state.data.flights.map((flight, i) => <TableHeadColumn key = {i} data = {flight} />)}
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.selectedDays.map((day, i) => <TableRow key = {i} data = {this.state.data} selectedDay = {day} />)}
+					</tbody>
+				</Table>
+			</div>
+		);
 	}
 }
 
 class TableHeadColumn extends React.Component {
-   render() {
-	  var myStyle = {
-         textAlign:'center'
-      }
-      return (
-         <th style={myStyle}>
-            {this.props.data.tee_time}
-         </th>
-      );
-   }
+	render() {
+		var myStyle = {
+			textAlign:'center'
+		}
+		return (
+			<th style={myStyle}>
+				{this.props.data.tee_time}
+			</th>
+		);
+	}
+}
+
+class TableRow extends React.Component {
+	render() {
+		return (
+			<tr>
+				<TableRowHead data = {this.props.selectedDay.day + " - " + this.props.selectedDay.date} />
+				{this.props.data.flights.map((flight, i) => <TableRowColumn key = {i} data = {flight} />)}
+			</tr>
+		);
+	}
 }
 
 class TableRowHead extends React.Component {
-   render() {
-	  var myStyle = {
-         textAlign:'center'
-      }
-      return (
-         <th style={myStyle}>
-            {this.props.headerProp}
-         </th>
-      );
-   }
+	render() {
+		var myStyle = {
+			textAlign:'center'
+		}
+		return (
+			<th style={myStyle}>
+				{this.props.data}
+			</th>
+		);
+	}
 }
 
 class TableRowColumn extends React.Component {
-   render() {
-      var myStyle = {
-         background: '#FFFFFF',
-		 padding: 0
-      }
-	  
-	  if(this.props.data.reserve_status != null && this.props.data.reserve_status == 1) {
-		  myStyle = {
-			 background: '#FF0000',
-			 padding: 0
-		  }
-	  }
-	  else if(this.props.data.reserve_status != null && this.props.data.reserve_status == 2) {
-		  myStyle = {
-			 background: '#FFFF00',
-			 padding: 0
-		  }
-	  }
-	  
-      return (
-         <td style={myStyle}>
-            <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={<Popover title="Popover bottom"><strong>Holy guacamole!</strong> Check this info.</Popover>}>
-			  <a role='button' />
-			</OverlayTrigger>
-         </td>
-      );
-   }
+	render() {
+		var myStyle = {
+			background: '#FFFFFF',
+			padding: 0
+		}
+
+		if(this.props.data.reserve_status != null) {
+			if(this.props.data.reserve_status == 1) {
+				myStyle = {
+					background: '#FF0000',
+					padding: 0
+				}
+			}
+			else if(this.props.data.reserve_status == 2) {
+				myStyle = {
+					background: '#FFFF00',
+					padding: 0
+				}
+			}
+
+			return (
+				<td style={myStyle}>
+					<OverlayTrigger trigger="click" rootClose placement="bottom" overlay={<Popover title="Popover bottom"><strong>Holy guacamole!</strong><ReserveInfo data = {this.props.data.user_reservation} /></Popover>}>
+						<a role='button' />
+					</OverlayTrigger>
+				</td>
+			);
+		}
+
+		return (
+			<td style={myStyle}>
+				<OverlayTrigger trigger="click" rootClose placement="bottom" overlay={<Popover title="Popover bottom"><strong>Available for booking!</strong> Available!!! <a>Book Now</a></Popover>}>
+					<a role='button' />
+				</OverlayTrigger>
+			</td>
+		);
+	}
+}
+
+class ReserveInfo extends React.Component {
+	render() {
+		return (
+			<div>
+				<table>
+					<tr>
+						<td>Name</td>
+						<td>{this.props.data.user.name}</td>
+					</tr>
+					<tr>
+						<td>{this.props.data.count_pax} Person(s)</td>
+						<td>{this.props.data.actual_pax}</td>
+					</tr>
+					<tr>
+						<td>{this.props.data.count_buggy} Buggy(s)</td>
+						<td>{this.props.data.actual_buggy}</td>
+					</tr>
+					<tr>
+						<td>{this.props.data.count_caddy} Caddy(s)</td>
+						<td>{this.props.data.actual_caddy}</td>
+					</tr>
+					<tr>
+						<td>{this.props.data.count_insurance} Insurance(s)</td>
+						<td>{this.props.data.actual_insurance}</td>
+					</tr>
+					<tr>
+						<td>Total</td>
+						<td>{this.props.data.actual_pax+this.props.data.actual_buggy+this.props.data.actual_caddy+this.props.data.actual_insurance}</td>
+					</tr>
+				</table>
+			</div>
+		);
+	}
 }
 
 export default App;
